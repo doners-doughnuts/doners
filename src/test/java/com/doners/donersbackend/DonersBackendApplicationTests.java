bc110417661b5df4d3c03ca0a8e0fc576c2d0eb0
손창현 <gganzii1215@gmail.com>
2022-03-14 15:03:28 +0900

    add: User Entity

--- src/test/java/com/doners/donersbackend/DonersBackendApplicationTests.java
+++ src/test/java/com/doners/donersbackend/DonersBackendApplicationTests.java
@@ -1,13 +1,35 @@
 package com.doners.donersbackend;
 
+import com.doners.donersbackend.db.entity.User;
+import com.doners.donersbackend.db.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @SpringBootTest
+@Rollback(value=false)
 class DonersBackendApplicationTests {
+
+	@Autowired
+	UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
+		User user = User.builder()
+				.userName("손창현")
+				.userNickname("웅대디")
+				.userEmail("gganzii1215@gmail.com")
+				.userAccount(null).build();
+
+		userRepository.save(user);
+
+		User user1 = userRepository.findByUserEmail("gganzii1215@gmail.com").get();
+
+		System.out.println(user1);
 	}
 
 }
