38cf87bb126e09437e545bb1e17f3eb841daa113
손창현 <gganzii1215@gmail.com>
2022-03-14 15:01:49 +0900

    [S06P22A404-64] init: spring boot project

---
+++ doners-backend/src/main/java/com/doners/donersbackend/DonersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.doners.donersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonersBackendApplication.class, args);
+	}
+
+}
