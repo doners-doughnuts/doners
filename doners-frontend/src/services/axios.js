cb2cc18fbbd653fa9f18effc81833078f3c5c554
eazae <xynezae@gmail.com>
2022-03-14 18:30:49 +0900

    [S06P22A404-129] init: axios instance

---
+++ doners-frontend/src/services/axios.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_DEV_URL,
+  Headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+/* Apply Interceptor */
+// HTTP request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    //
+    if (user && user.accessToken) {
+      // For Spring Boot back-end
+      config.headers.Authorization = 'Bearer ' + user.accessToken;
+      // for Node.js Express back-end
+      //// return { 'x-access-token': user.accessToken };
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// HTTP response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response) {
+      switch (error.response.status) {
+        /* 'JWT expired' exeption */
+        case 401:
+          // console.log(error.response);
+          break;
+        default:
+      }
+    } else {
+      // ex. 서버 키지 않은 경우
+    }
+    return Promise.reject(error.response.data);
+  }
+);
+
+export default instance;
