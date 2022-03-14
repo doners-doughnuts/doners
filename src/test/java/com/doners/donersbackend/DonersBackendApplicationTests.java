05d5e033660134b54f35e78a99fa4bdd3ffe5e88
손창현 <gganzii1215@gmail.com>
2022-03-14 15:01:49 +0900

    [S06P22A404-64] init: spring boot project

---
+++ src/test/java/com/doners/donersbackend/DonersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doners.donersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
