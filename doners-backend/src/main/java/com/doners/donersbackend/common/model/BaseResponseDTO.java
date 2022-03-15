aee8dd08c73939c46bd6e40146162afa102d2c33
손창현 <gganzii1215@gmail.com>
2022-03-15 12:00:37 +0900

    [S06P22A404-35] feat: profile image POST API

--- doners-backend/src/main/java/com/doners/donersbackend/common/model/BaseResponseDTO.java
+++ doners-backend/src/main/java/com/doners/donersbackend/common/model/BaseResponseDTO.java
@@ -8,22 +8,22 @@ import lombok.Getter;
 @Builder
 @Getter
 @ApiModel("BaseResponseDto")
-public class BaseResponseDto {
+public class BaseResponseDTO {
     @ApiModelProperty(name="응답 메세지", example="성공")
     String message;
 
     @ApiModelProperty(name="응답 코드", example="200")
     Integer statusCode;
 
-    public BaseResponseDto() {}
+    public BaseResponseDTO() {}
 
-    public BaseResponseDto(String message, Integer statusCode) {
+    public BaseResponseDTO(String message, Integer statusCode) {
         this.message = message;
         this.statusCode = statusCode;
     }
 
-    public static BaseResponseDto of(String message, Integer statusCode) {
-        return BaseResponseDto.builder()
+    public static BaseResponseDTO of(String message, Integer statusCode) {
+        return BaseResponseDTO.builder()
                 .message(message)
                 .statusCode(statusCode).build();
     }
