10f762941e1467d7798c3cdd246ba8f02062e6d5
kibum414 <kibum414@naver.com>
2022-03-17 01:30:39 +0900

    [S06P22A404-48] fix: register donation budget

--- doners-backend/src/main/java/com/doners/donersbackend/api/dto/request/DonationBudgetRequestDTO.java
+++ doners-backend/src/main/java/com/doners/donersbackend/api/dto/request/DonationBudgetRequestDTO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@ApiModel("DonationBudgetInfo")
+@ApiModel("DonationBudgetRequestInfo")
 @Getter
 @ToString
 @NoArgsConstructor
