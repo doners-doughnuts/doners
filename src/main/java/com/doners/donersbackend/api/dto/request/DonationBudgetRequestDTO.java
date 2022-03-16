6cbd840c7e514e158f339a697c306965a17817bd
kibum414 <kibum414@naver.com>
2022-03-17 00:23:05 +0900

    [S06P22A404-48] feat: register plan to use money

---
+++ src/main/java/com/doners/donersbackend/api/dto/request/DonationBudgetRequestDTO.java
@@ -0,0 +1,21 @@
+package com.doners.donersbackend.api.dto.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ApiModel("DonationBudgetInfo")
+@Getter
+@ToString
+@NoArgsConstructor
+public class DonationBudgetRequestDTO {
+
+    @ApiModelProperty(name = "활용 계획")
+    private String plan;
+
+    @ApiModelProperty(name = "활용 금액")
+    private long amount;
+
+}
