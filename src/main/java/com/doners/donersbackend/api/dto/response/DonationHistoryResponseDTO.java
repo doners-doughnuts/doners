b10f164895074d24155623c23202dfeeeaccc373
kibum414 <kibum414@naver.com>
2022-03-17 22:10:18 +0900

    [S06P22A404-51] feat: get donation

---
+++ src/main/java/com/doners/donersbackend/api/dto/response/DonationHistoryResponseDTO.java
@@ -0,0 +1,26 @@
+package com.doners.donersbackend.api.dto.response;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ApiModel("DonationHistoryResponseDTO")
+@Getter
+@ToString
+public class DonationHistoryResponseDTO {
+
+    @ApiModelProperty(name = "기부자 닉네임")
+    private String nickname;
+
+    @ApiModelProperty(name = "기부 금액")
+    private long amount;
+
+    @Builder
+    public DonationHistoryResponseDTO(String nickname, long amount) {
+        this.nickname = nickname;
+        this.amount = amount;
+    }
+
+}
--- src/main/java/com/doners/donersbackend/db/repository/FileRepository.java
+++ src/main/java/com/doners/donersbackend/db/repository/FileRepository.java
@@ -1,9 +1,16 @@
 package com.doners.donersbackend.db.repository;
 
+import com.doners.donersbackend.db.entity.donation.Donation;
 import com.doners.donersbackend.db.entity.donation.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FileRepository extends JpaRepository<File, String> {
+
+    Optional<List<File>> findByDonation(Donation donation);
+
 }
