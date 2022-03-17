e8e702a6672d8d8c67e511fab6cf03392ad3fd31
thdalstn6352 <thdalstn6352@naver.com>
2022-03-17 22:45:26 +0900

    [S06P22A404-77] feat: landing page

--- doners-frontend/src/App.tsx
+++ doners-frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Main from './routes/Main';
+import styles from './App.module.css';
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ doners-frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,152 @@
+import styles from './HomePage.module.css';
+import character from 'assets/images/character.png';
+
+const HomePage = () => {
+  return (
+    <div>
+      <section className={styles.Introduction}>
+        <div className={`${styles.container} ${styles['vertical-center']}`}>
+          <div className={styles['Introduction-box']}>
+            <h1 className={styles.slogan}>
+              Donate & Collect <br />
+              extraordinary <br />
+              <span>doughnut</span> NFTs
+            </h1>
+            <div className={styles.description}>
+              <span>
+                Donate to the ones that are in need. <br />
+                Doughnuts are on us.
+              </span>
+            </div>
+            <div className={styles.buttonRow}>
+              <button
+                className={`${styles['primary-button']} ${styles.button}`}
+              >
+                기부하기
+              </button>
+            </div>
+            <div className={styles['character-box']}>
+              <img
+                src={character}
+                alt="character"
+                className={styles.character}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.Services}>
+        <div className={styles.badge}>
+          <div className={styles['service-title']}>Why Doners?</div>
+        </div>
+        <div className={`${styles.container} ${styles['container-default']}`}>
+          <div className={styles.innerContainer}>
+            <h1 className={styles.title}>서비스 장점 1</h1>
+            <div className={styles.service}>
+              <span>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est.
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.Services}>
+        <div
+          className={`${styles.container} ${styles['container-default']} ${styles['right-sort']}`}
+        >
+          <div className={styles.innerContainer}>
+            <h1 className={styles.title}>서비스 장점 2</h1>
+            <div className={styles.service}>
+              <span>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est.
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.Services}>
+        <div className={`${styles.container} ${styles['container-default']}`}>
+          <div className={styles.innerContainer}>
+            <h1 className={styles.title}>서비스 장점 3</h1>
+            <div className={styles.service}>
+              <span>
+                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+                aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est.
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={`${styles.Cummunity} ${styles['vertical-center']}`}>
+        <div className={`${styles.container} ${styles['container-default']}`}>
+          <div className={`${styles.innerContainer} `}>
+            <div className={styles['welcome-community-box']}>
+              <div>
+                <img
+                  src={character}
+                  alt="character"
+                  className={styles['community-character']}
+                />
+              </div>
+              <div className={styles['community-contents']}>
+                <div className={styles['community-textbox']}>
+                  Join Our Community!
+                </div>
+                <div className={styles['right-button']}>
+                  <button
+                    className={`${styles['alternative-button']} ${styles.button}`}
+                  >
+                    Our Meet
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.About}>
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate1} ${styles['team-character']}`}
+        />
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate2} ${styles['team-character']}`}
+        />
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate3} ${styles['team-character']}`}
+        />
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate4} ${styles['team-character']}`}
+        />
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate5} ${styles['team-character']}`}
+        />
+        <img
+          src={character}
+          alt="character"
+          className={`${styles.teammate6} ${styles['team-character']}`}
+        />
+        <div className={styles.background}></div>
+        <div>
+          <span className={styles.teamName}>든킨</span>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
