3ce387d7cc6b84edb4a42019fd234265fd92105c
손창현 <gganzii1215@gmail.com>
2022-03-21 14:59:07 +0900

    [S06P22A404-149] feat: user login GET API

--- src/main/java/com/doners/donersbackend/api/controller/UserController.java
+++ src/main/java/com/doners/donersbackend/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.doners.donersbackend.api.controller;
 
-import com.doners.donersbackend.api.dto.request.UserInfoPostDTO;
+import com.doners.donersbackend.api.dto.request.UserInfoSetRequestDTO;
+import com.doners.donersbackend.api.dto.request.UserNicknameChangeRequestDTO;
+import com.doners.donersbackend.api.dto.response.UserLoginResponseDTO;
 import com.doners.donersbackend.api.service.AwsS3Service;
 import com.doners.donersbackend.api.service.UserService;
 import com.doners.donersbackend.common.model.BaseResponseDTO;
@@ -24,18 +26,18 @@ public class UserController {
     private final AwsS3Service awsS3Service;
 
     @PostMapping
-    @ApiOperation(value="필수 회원정보 입력 - 이름, 이메일, 닉네임")
+    @ApiOperation(value="필수 회원정보 입력 - 이름, 닉네임, 이메일, 메타마스크 계정 주소")
     @ApiResponses({
             @ApiResponse(code=201, message="필수 회원정보 입력에 성공했습니다."),
-            @ApiResponse(code=409, message="필수 회원정보 입력에 실패했습니다. / 이미 해당 이메일로 가입된 계정이 존재합니다."),
+            @ApiResponse(code=409, message="필수 회원정보 입력에 실패했습니다. / 이미 해당 이메일이나 메타마스크 계정 주소로 가입된 계정이 존재합니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> setUserInfo(
-            @RequestBody @Valid @ApiParam(value="필수 회원 정보", required=true) UserInfoPostDTO userInfoPostDTO) {
+            @RequestBody @Valid @ApiParam(value="필수 회원 정보", required=true) UserInfoSetRequestDTO userInfoSetRequestDTO) {
         try {
-            Integer statusCode = userService.setUserInfo(userInfoPostDTO);
+            Integer statusCode = userService.setUserInfo(userInfoSetRequestDTO);
 
             if(statusCode == 409) {
-                return ResponseEntity.status(409).body(BaseResponseDTO.of("이미 해당 이메일로 가입된 계정이 존재합니다.", 409));
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("이미 해당 이메일이나 메타마스크 계정 주소로 가입된 계정이 존재합니다.", 409));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,8 +47,36 @@ public class UserController {
         return ResponseEntity.status(201).body(BaseResponseDTO.of("필수 회원정보 입력에 성공했습니다.", 201));
     }
 
+    @GetMapping("/{userAccount}")
+    @ApiOperation(value="로그인")
+    @ApiResponses({
+            @ApiResponse(code=200, message="로그인에 성공했습니다."),
+            @ApiResponse(code=404, message="해당 메타마스크 계정 주소로 가입된 정보가 없습니다."),
+            @ApiResponse(code=409, message="로그인에 실패했습니다."),
+    })
+    public ResponseEntity<? extends BaseResponseDTO> login(
+            @PathVariable("userAccount") @ApiParam(value="메타마스크 계정 주소", required=true) String userAccount) {
+        UserLoginResponseDTO userLoginResponseDTO = null;
 
-    @PatchMapping("{userNickname}")
+        try {
+            userLoginResponseDTO = userService.getUserLoginResponseDTO(userAccount);
+
+            if(userLoginResponseDTO == null) {
+                return ResponseEntity.status(409).body(BaseResponseDTO.of("로그인에 실패했습니다.", 201));
+            }
+
+            // 추후 적용
+//            String accessToken = ;
+//            userLoginResponseDTO.changeAccessToken(accessToken);
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(BaseResponseDTO.of("해당 메타마스크 계정 주소로 가입된 정보가 없습니다.", 404));
+        }
+
+        return ResponseEntity.status(200).body(UserLoginResponseDTO.of("로그인에 성공했습니다.", 200, userLoginResponseDTO));
+    }
+
+
+    @PatchMapping("/nickname")
     @ApiOperation(value="닉네임 변경")
     @ApiResponses({
             @ApiResponse(code=200, message="닉네임 변경에 성공했습니다."),
@@ -54,7 +84,9 @@ public class UserController {
             @ApiResponse(code=409, message="닉네임 변경에 실패했습니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> changeNickname(
-            @PathVariable("userNickname") @ApiParam(value="닉네임", required=true) String userNickname) {
+//            @PathVariable("userNickname") @ApiParam(value="닉네임", required=true) String userNickname) {
+            @RequestBody @ApiParam(value="변경할 닉네임", required=true) UserNicknameChangeRequestDTO userNicknameChangeRequestDTO) {
+        String userNickname = userNicknameChangeRequestDTO.getUserNickname();
 
         try {
             Integer statusCode = userService.changeUserNickname(userNickname);
