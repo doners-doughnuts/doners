2d7bc08a4aed3d322c4c28da48ac1bdac478e3a2
sunmin <sunmlee126@gmail.com>
2022-03-22 02:19:44 +0900

    feat: header throttle
    - Apply throttle to header
    - Add logo font

--- doners-frontend/src/components/Header/Header.tsx
+++ doners-frontend/src/components/Header/Header.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import Logo from 'assets/images/logo.png';
 
-
 const Header = () => {
+  const [ScrollY, setScrollY] = useState(0); // window 의 pageYOffset값을 저장
+  const [ScrollActive, setScrollActive] = useState(false);
+
+  function handleScroll() {
+    if (ScrollY > 45) {
+      setScrollY(window.pageYOffset);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setScrollActive(false);
+    }
+  }
+
+  useEffect(() => {
+    function scrollListener() {
+      window.addEventListener('scroll', handleScroll);
+    } //  window 에서 스크롤을 감시 시작
+    scrollListener(); // window 에서 스크롤을 감시
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }; //  window 에서 스크롤을 감시를 종료
+  });
+
   return (
-    <div className={styles.header}>
+    <div
+      className={
+        ScrollActive
+          ? [styles.header, styles.fixed].join(' ')
+          : styles['header']
+      }
+    >
       <div className={styles['header-leftside']}>
         <ul className={styles['header-list']}>
           <Link to="/">
@@ -25,13 +53,20 @@ const Header = () => {
             <img src={Logo} className={styles.logoImg} alt="logo" />
           </Link>
         </div>
+        <Link to="/">
+          <div className={styles.logoTitle}>DONERS</div>
+        </Link>
       </div>
       <div className={styles['header-rightside']}>
         <ul className={styles['header-list']}>
           <li>언어</li>
           <li>알림</li>
           <li>프로필</li>
-          <button>Join</button>
+          <button
+            className={`${styles['alternative-button']} ${styles.button}`}
+          >
+            Join
+          </button>
         </ul>
       </div>
     </div>
