3f6ccbc9b0dc22ff1c81b1e39405724963bede82
ziu974 <ziu974@gmail.com>
2022-03-21 21:16:42 +0900

    [S06P22A404-176] feat: add recoil

--- doners-frontend/src/App.tsx
+++ doners-frontend/src/App.tsx
@@ -1,14 +1,16 @@
+import { RecoilRoot } from 'recoil';
 import Router from 'routes/routes';
 // TODO 
 import styles from './App.module.css';
 
 export default function App() {
-  return (
-    // <ScrollToTop />
-    // <GlobalStyles />
-    // {/* Recoil을 사용하는 component들은 <RecoilRoot>로 감싸야 함 */}
-    // <div className={styles.App}>
-    <Router />
-    // </div>
-  );
+  return (<>
+    {/* <ScrollToTop />
+     <GlobalStyles /> */}
+    {/* Recoil을 사용하는 component들은 <RecoilRoot>로 감싸야 함 */}
+    < RecoilRoot >
+      <Router />
+    </RecoilRoot >
+  </>
+  )
 }
