2a1be4d4e4c2745afb59e26691639c68d632b4f9
kibum414 <kibum414@naver.com>
2022-03-22 10:27:09 +0900

    [S06P22A404-184] feat: donation budget validation check

--- doners-backend/src/main/java/com/doners/donersbackend/api/dto/request/DonationBudgetRequestDTO.java
+++ doners-backend/src/main/java/com/doners/donersbackend/api/dto/request/DonationBudgetRequestDTO.java
@@ -6,15 +6,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 @ApiModel("DonationBudgetRequestInfo")
 @Getter
 @ToString
 @NoArgsConstructor
 public class DonationBudgetRequestDTO {
 
+    @NotBlank
     @ApiModelProperty(name = "활용 계획")
     private String plan;
 
+    @Positive
     @ApiModelProperty(name = "활용 금액")
     private long amount;
 
