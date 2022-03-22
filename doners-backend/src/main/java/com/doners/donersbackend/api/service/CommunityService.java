5040c60ad1449fca49d8a8fb3b00aec814a1cbc5
JeongHongJin <mistake9518@gmail.com>
2022-03-22 11:41:33 +0900

    [S06P22A404-182] feat: community detail api

--- doners-backend/src/main/java/com/doners/donersbackend/api/service/CommunityService.java
+++ doners-backend/src/main/java/com/doners/donersbackend/api/service/CommunityService.java
@@ -3,6 +3,7 @@ package com.doners.donersbackend.api.service;
 import com.doners.donersbackend.api.dto.request.CommunityChangePatchDTO;
 import com.doners.donersbackend.api.dto.request.CommunityRegisterPostDTO;
 import com.doners.donersbackend.api.dto.response.CommunityGetListWrapperResponseDTO;
+import com.doners.donersbackend.api.dto.response.CommunityResponseDTO;
 
 public interface CommunityService {
     // 글 작성 : 필수 글 정보 입력 - 제목, 내용
@@ -13,4 +14,6 @@ public interface CommunityService {
     Integer deleteCommunity(String communityId);
     // 커뮤니티 글 목록 조회
     CommunityGetListWrapperResponseDTO getCommunityList();
+    // 커뮤니티 글 목록 조회
+    CommunityResponseDTO getCommunity(String communityId);
 }
