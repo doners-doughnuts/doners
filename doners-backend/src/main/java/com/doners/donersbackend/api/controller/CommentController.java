0f0530f30a873e37640de65e7220191374a15e6a
JeongHongJin <mistake9518@gmail.com>
2022-03-23 19:48:13 +0900

    [S06P22A404-57] feat: subcomment register change delete

--- doners-backend/src/main/java/com/doners/donersbackend/api/controller/CommentController.java
+++ doners-backend/src/main/java/com/doners/donersbackend/api/controller/CommentController.java
@@ -90,26 +90,35 @@ public class CommentController {
     }
 
     @GetMapping("/community/{communityId}")
-    @ApiOperation(value="필수 댓글 정보 입력 - 내용,글 id ,작성자 유저 주소")
+    @ApiOperation(value="필수 댓글 정보 입력 - 글 id")
     @ApiResponses({
-            @ApiResponse(code=201, message="필수 댓글 정보 입력에 성공했습니다."),
-            @ApiResponse(code=409, message="필수 댓글 정보 입력에 실패했습니다.")
+            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> getCommunityComment(
             @PathVariable("communityId") @ApiParam(value="글id", required=true) String communityId) {
 
-        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"커뮤니티 댓글 목록 조회 성공", commentService.getCommunityCommentList(communityId));
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"커뮤니티 댓글 목록 조회 성공", commentService.getCommunityCommentList(communityId)));
     }
 
     @GetMapping("/appreciation/{appreciationId}")
-    @ApiOperation(value="필수 댓글 정보 입력 - 내용,글 id ,작성자 유저 주소")
+    @ApiOperation(value="필수 댓글 정보 입력 - 글 id")
     @ApiResponses({
-            @ApiResponse(code=201, message="필수 댓글 정보 입력에 성공했습니다."),
-            @ApiResponse(code=409, message="필수 댓글 정보 입력에 실패했습니다.")
+            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> getAppreciationComment(
             @PathVariable("appreciationId") @ApiParam(value="글id", required=true) String appreciationId) {
 
-        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"감사 글 댓글 목록 조회 성공", commentService.getAppreciationCommentList(appreciationId));
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"감사 글 댓글 목록 조회 성공", commentService.getAppreciationCommentList(appreciationId)));
+    }
+
+    @GetMapping("/subComment/{commentId}")
+    @ApiOperation(value="필수 댓글 정보 입력 - 댓글 id")
+    @ApiResponses({
+            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
+    })
+    public ResponseEntity<? extends BaseResponseDTO> getSubComment(
+            @PathVariable("commentId") @ApiParam(value="댓글 id", required=true) String commentId) {
+
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"대댓글 목록 조회 성공", commentService.getSubCommentList(commentId)));
     }
 }
