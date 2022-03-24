11cb6f0a34bd27f720cacc7889bf4e24f918bb57
kibum414 <kibum414@naver.com>
2022-03-25 02:48:17 +0900

    [S06P22A404-171] feat: notify approval

---
+++ doners-backend/src/main/java/com/doners/donersbackend/domain/dao/donation/Notification.java
@@ -0,0 +1,34 @@
+package com.doners.donersbackend.domain.dao.donation;
+
+import com.doners.donersbackend.domain.dao.BaseEntity;
+import com.doners.donersbackend.domain.enums.NotificationCode;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Notification extends BaseEntity {
+
+    @Column(name = "notification_description")
+    private String description;
+
+    @Column(name = "notification_is_read", columnDefinition = "BOOLEAN DEFAULT false")
+    private boolean isRead;
+
+    @Column(name = "notification_code")
+    private NotificationCode notificationCode;
+
+    @Column(name = "notification_create_time")
+    private LocalDateTime createTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "donation_id")
+    private Donation donation;
+
+}
