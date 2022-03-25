e997b79f5ad92f889ffaba9ce345ee19ba66ac27
kibum414 <kibum414@naver.com>
2022-03-25 16:34:52 +0900

    feat: authentication using jwt for donation

--- src/main/java/com/doners/donersbackend/application/service/DonationService.java
+++ src/main/java/com/doners/donersbackend/application/service/DonationService.java
@@ -14,7 +14,7 @@ import java.util.List;
 public interface DonationService {
 
     // 기부글 신청
-    Boolean createDonation(DonationInfoRequestDTO donationInfoRequestDTO, MultipartFile certificate, MultipartFile image, List<MultipartFile> evidence);
+    Boolean createDonation(String accessToken, DonationInfoRequestDTO donationInfoRequestDTO, MultipartFile certificate, MultipartFile image, List<MultipartFile> evidence);
 
     // 기부글 목록 조회
     DonationGetListWrapperResponseDTO getDonationList(CategoryCode categoryCode);
@@ -23,13 +23,13 @@ public interface DonationService {
     DonationResponseDTO getDonation(String donationId);
 
     // 기부글 추천
-    DonationRecommendResponseDTO recommendDonation(String donationId);
+    DonationRecommendResponseDTO recommendDonation(String accessToken, String donationId);
 
     // 기부글 검색
     DonationGetListWrapperResponseDTO searchDonation(String type, String keyword);
 
     // 기부글 승인
-    Integer approveDonation(DonationApproveRequestDTO donationApproveRequestDTO) throws NullPointerException;
+    Integer approveDonation(String accessToken, DonationApproveRequestDTO donationApproveRequestDTO) throws NullPointerException;
 
     // 대표 사진 및 증빙 자료 업로드
     void uploadDonationFile(Donation donation, MultipartFile image, List<MultipartFile> evidence);
