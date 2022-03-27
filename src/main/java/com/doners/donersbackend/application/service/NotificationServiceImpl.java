d7ff35492fd2e4b40de31058de9e85283ac38a4f
kibum414 <kibum414@naver.com>
2022-03-28 01:12:22 +0900

    feat: read notification

--- src/main/java/com/doners/donersbackend/application/service/NotificationServiceImpl.java
+++ src/main/java/com/doners/donersbackend/application/service/NotificationServiceImpl.java
@@ -1,9 +1,11 @@
 package com.doners.donersbackend.application.service;
 
+import com.doners.donersbackend.application.dto.request.donation.NotificationReadDTO;
 import com.doners.donersbackend.application.dto.response.donation.NotificationResponseDTO;
-import com.doners.donersbackend.domain.dao.user.User;
 import com.doners.donersbackend.domain.dao.donation.Donation;
 import com.doners.donersbackend.domain.dao.donation.Notification;
+import com.doners.donersbackend.domain.dao.user.User;
+import com.doners.donersbackend.domain.enums.ApprovalStatusCode;
 import com.doners.donersbackend.domain.enums.NotificationCode;
 import com.doners.donersbackend.domain.repository.UserRepository;
 import com.doners.donersbackend.domain.repository.donation.DonationRepository;
@@ -34,30 +36,60 @@ public class NotificationServiceImpl implements NotificationService {
         String userAccount = jwtAuthenticationProvider.getUserAccount(token);
 
         User user = userRepository.findByUserAccountAndUserIsDeleted(userAccount, false)
-                .orElseThrow(() -> new IllegalArgumentException("유저 정보가 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("해당 계정을 찾을 수 없습니다."));
 
         // 회원의 삭제되지 않은 기부글
         Donation donation = donationRepository.findByUserAndIsDeleted(user, false)
                 .orElseThrow(() -> new IllegalArgumentException("해당 기부글을 찾을 수 없습니다."));
 
         // 종료 시간 지났으면
-        if (donation.getEndTime() != null && LocalDateTime.now().isAfter(donation.getEndTime())) return createNotification(donation, "종료", NotificationCode.PROGRESS);
+        if (donation.isApproved() && donation.getEndTime() != null && LocalDateTime.now().isAfter(donation.getEndTime())) {
+            return createNotification(donation, "종료", NotificationCode.PROGRESS);
+        }
 
-        // 승인되었으면
-        if (donation.isApproved()) return createNotification(donation, "승인", NotificationCode.APPROVAL);
+        // 승인된 상태라면
+        if (donation.getApprovalStatusCode().equals(ApprovalStatusCode.APPROVAL)) {
+            return createNotification(donation, "승인", NotificationCode.APPROVAL);
+        // 거절된 상태라면
+        } else if (donation.getApprovalStatusCode().compareTo(ApprovalStatusCode.APPROVAL) > 0) {
+            return createNotification(donation, "거절", NotificationCode.APPROVAL);
+        }
 
         return null;
 
     }
 
+    @Override
+    public void readNotification(String accessToken, NotificationReadDTO notificationReadDTO) {
+
+        String token = accessToken.split(" ")[1];
+
+        String userAccount = jwtAuthenticationProvider.getUserAccount(token);
+
+        User user = userRepository.findByUserAccountAndUserIsDeleted(userAccount, false)
+                .orElseThrow(() -> new IllegalArgumentException("해당 계정을 찾을 수 없습니다."));
+
+        Notification notification = notificationRepository.findById(notificationReadDTO.getNotificationId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 알림을 찾을 수 없습니다."));
+
+        notification.changeIsRead();
+
+        notificationRepository.save(notification);
+
+    }
+
     public NotificationResponseDTO createNotification(Donation donation, String status, NotificationCode notificationCode) {
 
-        // 읽지 않은 알림 존재 여부 확인
-        Notification notification = notificationRepository.findByDonationAndNotificationCodeAndIsRead(donation, notificationCode, false).orElse(null);
+        // 알림 존재 여부 확인
+        Notification notification = notificationRepository.findByDonationAndNotificationCode(donation, notificationCode).orElse(null);
 
         // 기존 알림이 있다면
         if (notification != null) {
+            // 알림을 읽었다면
+            if (notification.isRead()) return null;
+
             return NotificationResponseDTO.builder()
+                    .notificationId(notification.getId())
                     .donationId(donation.getId())
                     .description(notification.getDescription())
                     .createTime(notification.getCreateTime())
@@ -72,7 +104,10 @@ public class NotificationServiceImpl implements NotificationService {
                 .donation(donation)
                 .build();
 
+        notificationRepository.save(notificationInfo);
+
         return NotificationResponseDTO.builder()
+                .notificationId(notificationInfo.getId())
                 .donationId(donation.getId())
                 .description(notificationInfo.getDescription())
                 .createTime(notificationInfo.getCreateTime())
