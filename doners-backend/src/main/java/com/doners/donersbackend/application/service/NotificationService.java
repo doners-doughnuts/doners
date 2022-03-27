bc4e224eabc86954c4da7ed17c2568039c01ded6
kibum414 <kibum414@naver.com>
2022-03-28 01:12:22 +0900

    feat: read notification

--- doners-backend/src/main/java/com/doners/donersbackend/application/service/NotificationService.java
+++ doners-backend/src/main/java/com/doners/donersbackend/application/service/NotificationService.java
@@ -1,9 +1,12 @@
 package com.doners.donersbackend.application.service;
 
+import com.doners.donersbackend.application.dto.request.donation.NotificationReadDTO;
 import com.doners.donersbackend.application.dto.response.donation.NotificationResponseDTO;
 
 public interface NotificationService {
 
     NotificationResponseDTO getNotification(String accessToken);
+
+    void readNotification(String accessToken, NotificationReadDTO notificationReadDTO);
 
 }
