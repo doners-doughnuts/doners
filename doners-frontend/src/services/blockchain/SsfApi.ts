aac8f2631fa8f985f79ac851aa66ecff627793da
ziu974 <ziu974@gmail.com>
2022-03-29 03:05:22 +0900

    [S06P22A404-225] feat: add withdraw function

--- doners-frontend/src/services/blockchain/SsfApi.ts
+++ doners-frontend/src/services/blockchain/SsfApi.ts
@@ -1,4 +1,4 @@
-import { Web3Client } from 'services/web3';
+import { SsfContract, Web3Client } from 'services/web3';
 /**
  * 필요한 기능
  * [Doation(SSF)]
@@ -6,6 +6,8 @@ import { Web3Client } from 'services/web3';
  * - 지갑 balance
  * - 오늘 거래된 SSF 총향
  * - transaction stages (SSF)
+ * - 기부금 수령하기
+ * -
  */
 
 /* Wallet balance 조회 */
@@ -17,4 +19,9 @@ export const getSSFBalance = async (walletAddress: string) => {
 
   // const result = await SsfContract.methods.balanceOf(walletAddress).call(); // 29803630997051883414242659
   // const format = Web3Client.utils.fromWei(result); // 29803630.997051883414242659
+};
+
+/* 기부금 수령하기 */
+export const withdraw = async () => {
+  SsfContract.methods.withdraw().call().then(console.log);
 };
--- doners-frontend/src/services/web3.ts
+++ doners-frontend/src/services/web3.ts
@@ -1,6 +1,7 @@
 // ? [해결] 왜 .ts 파일에서는 import가 되지 않는가. js만 지원?
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
+import * as data from 'assets/abi/fundraiserABI.json';
 
 const HTTP_PROVIDER = 'http://20.196.209.2:8545';
 const WEBSOCKET_PROVIDER = 'ws://20.196.209.2:6174';
@@ -14,16 +15,305 @@ export const Web3Client = new Web3(
 // const Web3Client = new Web3(Web3.givenProvider || new Web3.providers.WebsocketProvider(WEBSOCKET_PROVIDER));
 
 // The minimum ABI required to get the ERC20 Token balance
+// const minABI: AbiItem | AbiItem[] = [
+//   // balanceOf
+//   {
+//     constant: true,
+//     inputs: [{ name: '_owner', type: 'address' }],
+//     name: 'balanceOf',
+//     outputs: [{ name: 'balance', type: 'uint256' }],
+//     type: 'function',
+//   },
+// ];
 const minABI: AbiItem | AbiItem[] = [
-  // balanceOf
   {
-    constant: true,
-    inputs: [{ name: '_owner', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ name: 'balance', type: 'uint256' }],
+    inputs: [
+      {
+        internalType: 'string',
+        name: '_name',
+        type: 'string',
+      },
+      {
+        internalType: 'string',
+        name: '_url',
+        type: 'string',
+      },
+      {
+        internalType: 'string',
+        name: '_imageURL',
+        type: 'string',
+      },
+      {
+        internalType: 'string',
+        name: '_description',
+        type: 'string',
+      },
+      {
+        internalType: 'address payable',
+        name: '_beneficiary',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: '_custodian',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'donor',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'DonationReceived',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'Withdraw',
+    type: 'event',
+  },
+  {
+    stateMutability: 'payable',
+    type: 'fallback',
+  },
+  {
+    inputs: [],
+    name: 'beneficiary',
+    outputs: [
+      {
+        internalType: 'address payable',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'custodian',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'description',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'donationsCount',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'imageURL',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalDonations',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'url',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address payable',
+        name: '_beneficiary',
+        type: 'address',
+      },
+    ],
+    name: 'setBeneficiary',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'myDonationsCount',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'donate',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'myDonations',
+    outputs: [
+      {
+        internalType: 'uint256[]',
+        name: 'values',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'dates',
+        type: 'uint256[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'withdraw',
+    outputs: [],
+    stateMutability: 'nonpayable',
     type: 'function',
   },
 ];
+// const ABI = require('./blockchain/abi.json');
 // todo const CONTRACT_ABI: AbiItem | AbiItem[] = [];
 
 const tokenAddress = '0x94B52272d82689d17785FF5956D099F56612Eba8';
