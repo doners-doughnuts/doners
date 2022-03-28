46cd23513073f8d15c833c549bcca1d1faf08c16
ziu974 <ziu974@gmail.com>
2022-03-28 16:27:09 +0900

    [S06P22A404-224] feat: SSAFY private network connect configuration

--- doners-frontend/src/App.tsx
+++ doners-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Router from 'routes/routes';
 // TODO
 import styles from './App.module.css';
 import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
+import Fest from 'services/nft/Fest';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       {/* Recoil을 사용하는 component들은 <RecoilRoot>로 감싸야 함 */}
       <RecoilRoot>
         <ScrollToTop />
+        <Fest />
 
         <Router />
       </RecoilRoot>
