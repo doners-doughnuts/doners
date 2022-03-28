e48d4baa4e271e0cd4843c3ee72d2a82d8678253
손창현 <gganzii1215@gmail.com>
2022-03-28 22:54:39 +0900

    refactor: comment

--- doners-backend/src/main/java/com/doners/donersbackend/api/controller/CommentController.java
+++ doners-backend/src/main/java/com/doners/donersbackend/api/controller/CommentController.java
@@ -30,7 +30,7 @@ public class CommentController {
     })
     public ResponseEntity<? extends BaseResponseDTO> registerComment(
             @ApiIgnore @RequestHeader("Authorization") String accessToken,
-            @RequestBody @Valid @ApiParam(value="필수 게시글 정보", required=true) CommentRegisterPostDTO commentRegisterPostDTO) {
+            @RequestBody @Valid @ApiParam(value="추가할 댓글 정보", required=true) CommentRegisterPostDTO commentRegisterPostDTO) {
         try {
             commentService.registerComment(accessToken, commentRegisterPostDTO);
         } catch (Exception e) {
@@ -41,7 +41,7 @@ public class CommentController {
         return ResponseEntity.status(201).body(BaseResponseDTO.of("필수 댓글 정보 입력에 성공했습니다.", 201));
     }
 
-    @PatchMapping("/{commentId}")
+    @PatchMapping
     @ApiOperation(value="댓글 변경 , 필수 정보 - 내용, 댓글 id")
     @ApiResponses({
             @ApiResponse(code=200, message="댓글 변경에 성공했습니다."),
@@ -49,33 +49,34 @@ public class CommentController {
             @ApiResponse(code=409, message="댓글 변경에 실패했습니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> changeComment(
-            @PathVariable("commentId") @ApiParam(value="댓글 id", required=true) String commentId,
-            @RequestBody @Valid @ApiParam(value="필수 게시글 정보", required=true) CommentChangePatchDTO commentChangePatchDTO) {
+            @ApiIgnore @RequestHeader("Authorization") String accessToken,
+            @RequestBody @Valid @ApiParam(value="수정할 댓글 내용", required=true) CommentChangePatchDTO commentChangePatchDTO) {
 
         try {
-            Integer statusCode = commentService.changeComment(commentId,commentChangePatchDTO);
+            Integer statusCode = commentService.changeComment(accessToken, commentChangePatchDTO);
 
             if(statusCode == 409)
                 return ResponseEntity.status(409).body(BaseResponseDTO.of("댓글 변경에 실패했습니다.", 409));
         } catch (IllegalArgumentException e){
-            return ResponseEntity.status(404).body(BaseResponseDTO.of("해당 댓글을 을 수 없습니다.", 404));
+            return ResponseEntity.status(404).body(BaseResponseDTO.of("해당 댓글을 찾을 수 없습니다.", 404));
         }
 
         return ResponseEntity.status(200).body(BaseResponseDTO.of("댓글 변경에 성공했습니다.", 200));
     }
 
     @DeleteMapping("/delete/{commentId}")
-    @ApiOperation(value="댓글 삭제 , 필수 정보 - 댓글 id")
+    @ApiOperation(value="댓글 삭제")
     @ApiResponses({
             @ApiResponse(code=200, message="댓글 삭제에 성공했습니다."),
             @ApiResponse(code=404, message="해당 댓글을 찾을 수 없습니다."),
             @ApiResponse(code=409, message="댓글 삭제에 실패했습니다."),
     })
-    public ResponseEntity<? extends BaseResponseDTO> deleteCommunity(
-            @PathVariable("commentId") @ApiParam(value="글id", required=true) String commentId) {
+    public ResponseEntity<? extends BaseResponseDTO> deleteComment(
+            @ApiIgnore @RequestHeader("Authorization") String accessToken,
+            @PathVariable("commentId") @ApiParam(value="댓글 ID", required=true) String commentId) {
 
         try {
-            Integer statusCode = commentService.deleteComment(commentId);
+            Integer statusCode = commentService.deleteComment(accessToken, commentId);
 
             if(statusCode == 409)
                 return ResponseEntity.status(409).body(BaseResponseDTO.of("댓글 삭제에 실패했습니다.", 409));
@@ -87,35 +88,38 @@ public class CommentController {
     }
 
     @GetMapping("/community/{communityId}")
-    @ApiOperation(value="필수 댓글 정보 입력 - 글 id")
+    @ApiOperation(value="커뮤니티 글 댓글 리스트 조회")
     @ApiResponses({
-            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
+            @ApiResponse(code=200, message="커뮤니티 글에 달린 댓글 리스트 조회에 성공했습니다."),
     })
-    public ResponseEntity<? extends BaseResponseDTO> getCommunityComment(
-            @PathVariable("communityId") @ApiParam(value="글id", required=true) String communityId) {
+    public ResponseEntity<? extends BaseResponseDTO> getCommunityCommentList(
+            @ApiIgnore @RequestHeader("Authorization") String accessToken,
+            @PathVariable("communityId") @ApiParam(value="커뮤니티 ID", required=true) String communityId) {
 
-        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"커뮤니티 댓글 목록 조회 성공", commentService.getCommunityCommentList(communityId)));
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"커뮤니티 댓글 목록 조회 성공", commentService.getCommunityCommentList(accessToken, communityId)));
     }
 
     @GetMapping("/epilogue/{epilogueId}")
-    @ApiOperation(value="필수 댓글 정보 입력 - 글 id")
+    @ApiOperation(value="에필로그 글 댓글 리스트 조회")
     @ApiResponses({
-            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
+            @ApiResponse(code=200, message="에필로그 글에 달린 댓글 리스트 조회에 성공했습니다."),
     })
-    public ResponseEntity<? extends BaseResponseDTO> getEpilogueComment(
-            @PathVariable("epilogueId") @ApiParam(value="글id", required=true) String epilogueId) {
+    public ResponseEntity<? extends BaseResponseDTO> getEpilogueCommentList(
+            @ApiIgnore @RequestHeader("Authorization") String accessToken,
+            @PathVariable("epilogueId") @ApiParam(value="에필로그 ID", required=true) String epilogueId) {
 
-        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"감사 글 댓글 목록 조회 성공", commentService.getEpilogueCommentList(epilogueId)));
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"감사 글 댓글 목록 조회 성공", commentService.getEpilogueCommentList(accessToken, epilogueId)));
     }
 
     @GetMapping("/subComment/{commentId}")
-    @ApiOperation(value="필수 댓글 정보 입력 - 댓글 id")
+    @ApiOperation(value="대댓글 리스트 조회")
     @ApiResponses({
-            @ApiResponse(code=200, message="필수 댓글 정보 조회에 성공했습니다."),
+            @ApiResponse(code=200, message="대댓글 리스트 조회에 성공했습니다."),
     })
     public ResponseEntity<? extends BaseResponseDTO> getSubComment(
-            @PathVariable("commentId") @ApiParam(value="댓글 id", required=true) String commentId) {
+            @ApiIgnore @RequestHeader("Authorization") String accessToken,
+            @PathVariable("commentId") @ApiParam(value="댓글 ID", required=true) String commentId) {
 
-        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"대댓글 목록 조회 성공", commentService.getSubCommentList(commentId)));
+        return ResponseEntity.ok(CommentGetListWrapperResponseDTO.of(200,"대댓글 목록 조회 성공", commentService.getSubCommentList(accessToken, commentId)));
     }
 }
