00ec2dac439e13691e7c9dc0e9f4baf5b8974f5a
손창현 <gganzii1215@gmail.com>
2022-03-29 11:36:58 +0900

    feat: include user information in comment response

--- doners-backend/src/main/java/com/doners/donersbackend/application/dto/response/comment/CommentResponseDTO.java
+++ doners-backend/src/main/java/com/doners/donersbackend/application/dto/response/comment/CommentResponseDTO.java
@@ -15,17 +15,21 @@ public class CommentResponseDTO {
     @ApiModelProperty(name = "댓글 ID")
     private String commentId;
 
-    @ApiModelProperty(name = "작성 시간")
-    private LocalDateTime commentCreateTime;
-
     @ApiModelProperty(name = "댓글 내용")
     private String commentDescription;
 
+    @ApiModelProperty(name = "작성자 닉네임")
+    private String nickname;
+
+    @ApiModelProperty(name = "작성 시간")
+    private LocalDateTime commentCreateTime;
+
     @Builder
-    public CommentResponseDTO(String commentId, LocalDateTime commentCreateTime,String commentDescription) {
+    public CommentResponseDTO(String commentId, LocalDateTime commentCreateTime, String commentDescription, String nickname) {
         this.commentId = commentId;
         this.commentCreateTime = commentCreateTime;
         this.commentDescription = commentDescription;
+        this.nickname = nickname;
     }
 
 }
