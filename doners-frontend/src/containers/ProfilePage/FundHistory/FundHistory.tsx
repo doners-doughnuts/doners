266aee2071ae17e7ec0e4dd97f06edb80e974bbc
sunmin <sunmlee126@gmail.com>
2022-03-30 01:05:39 +0900

    [S06P22A404-236] feat: profile - fundhistory component
    - my funding history basic version

--- doners-frontend/src/containers/ProfilePage/FundHistory/FundHistory.tsx
+++ doners-frontend/src/containers/ProfilePage/FundHistory/FundHistory.tsx
@@ -3,10 +3,30 @@ import classNames from 'classnames/bind';
 import styles from './FundHistory.module.scss';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import FundingItem from 'components/FundingItem/FundingItem';
+import LastFundingItem from 'components/LastFundingItem/LastFundingItem';
+import H3 from 'assets/theme/Typography/H3/H3';
 
 const cx = classNames.bind(styles);
 const FundHistory = () => {
-  return <div>FundHistory!</div>;
+  return (
+    <div>
+      <div>
+        <H3>신청한 모금</H3>
+        <FundingItem />
+      </div>
+      <hr />
+      <div>
+        <H3>이전 모금 내역</H3>
+        <div>
+          <LastFundingItem />
+        </div>
+        <div>
+          <LastFundingItem />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FundHistory;
