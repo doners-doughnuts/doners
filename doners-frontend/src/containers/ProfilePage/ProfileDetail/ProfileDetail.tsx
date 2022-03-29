469b910792765e17bcc906d041903e002f61d398
sunmin <sunmlee126@gmail.com>
2022-03-29 15:09:33 +0900

    feat: profile my NFT tab component

--- doners-frontend/src/containers/ProfilePage/ProfileDetail/ProfileDetail.tsx
+++ doners-frontend/src/containers/ProfilePage/ProfileDetail/ProfileDetail.tsx
@@ -22,7 +22,6 @@ const ProfileDetail = ({ focus }: ProfileType) => {
             <ProfileTab focus={focus} />
             <div className={cx('inner-container')}>
               <main className={cx('content')}>
-                ProfileDetail!{focus}
                 {focus === 1 ? (
                   <div>
                     <MyNFT />
