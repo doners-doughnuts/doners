5180ffa0f59a26c6538741137778c35c39bd9699
sunmin <sunmlee126@gmail.com>
2022-03-29 15:31:03 +0900

    Revert "feat: profile my NFT tab component"
    This reverts commit 3968e023d06eb368a00d1b82f76a4147bde497aa.

--- doners-frontend/src/containers/ProfilePage/MyNFT/MyNFT.tsx
+++ doners-frontend/src/containers/ProfilePage/MyNFT/MyNFT.tsx
@@ -4,36 +4,16 @@ import styles from './MyNFT.module.scss';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import NFTCard from 'components/NFTCard/NFTCard';
-import EpilogueCard from 'components/EpilogueCard/EpilogueCard';
 
 const cx = classNames.bind(styles);
 const MyNFT = () => {
   return (
     <div>
-      <section className={cx('container')}>
-        <div className={cx('row')}>
-          <div className={cx('col-lg')}>
-            <Link to="1">
-              <NFTCard />
-            </Link>
-          </div>
-          <div className={cx('col-lg')}>
-            <NFTCard />
-          </div>
-          <div className={cx('col-lg')}>
-            <NFTCard />
-          </div>
-          <div className={cx('col-lg')}>
-            <NFTCard />
-          </div>
-          <div className={cx('col-lg')}>
-            <NFTCard />
-          </div>
-          <div className={cx('col-lg')}>
-            <NFTCard />
-          </div>
-        </div>
-      </section>
+      <NFTCard />
+      mynft!<div>mynft!</div>
+      <div>mynft!</div>
+      <div>mynft!</div>
+      <div>mynft!</div>
     </div>
   );
 };
