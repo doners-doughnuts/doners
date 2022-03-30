d5e10c7c45f37e33683a66eb15be741db73d72e3
sunmin <sunmlee126@gmail.com>
2022-03-30 11:02:03 +0900

    [S06P22A404-190] feat: profile - fundmodal component

--- doners-frontend/src/containers/ProfilePage/FundModal/FundModal.tsx
+++ doners-frontend/src/containers/ProfilePage/FundModal/FundModal.tsx
@@ -1,48 +1,32 @@
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
-import styles from './ProfileModal.module.scss';
+import styles from './FundModal.module.scss';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Button from 'assets/theme/Button/Button';
 import Input from 'assets/theme/Input/Input';
 import { checkNickname } from 'services/api/UserApi';
 import Avatar from 'assets/theme/Avatar/Avatar';
+import Span from 'assets/theme/Typography/Span/Span';
+import Progressbar from 'assets/theme/Progressbar/Progressbar';
+import H4 from 'assets/theme/Typography/H4/H4';
+import H2 from 'assets/theme/Typography/H2/H2';
+import H3 from 'assets/theme/Typography/H3/H3';
 const cx = classNames.bind(styles);
 type ProfileType = {
   focus: number;
   // user: string;
 };
-const ProfileModal = (props: { open?: any; close?: any }) => {
+const FundModal = (props: { open?: any; close?: any }) => {
   const { open, close } = props;
+  const [target, setTarget] = useState(3.89);
+  const [totalpeople, setTotalpeople] = useState(234);
+  const [current, setCurrent] = useState(1.0);
   const [nickname, setNickname] = useState('');
   const [nicknameMsg, setNicknameMsg] = useState('');
   const [nicknameConfirm, setNicknameConfirm] = useState(false);
   const [nicknameCheck, setNicknameCheck] = useState(false);
-
-  const handleInput = (event: { target: { id: any; value: any } }) => {
-    const nicknamePattern = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
-    const { id: target, value } = event.target;
-    switch (target) {
-      case 'nickname':
-        setNickname(value);
-        if (value.length < 2 && value.length > 0) {
-          setNicknameMsg('2자 이상의 닉네임을 입력해주세요.');
-        } else if (value.length > 10) {
-          setNicknameMsg('10자 이하의 닉네임을 입력해주세요.');
-        } else if (value === '') {
-          setNicknameMsg('닉네임을 입력해주세요');
-        } else if (!nicknamePattern.test(value)) {
-          setNicknameMsg('닉네임은 특수기호가 불가능합니다.');
-        } else {
-          setNicknameMsg('');
-          setNicknameCheck(true);
-        }
-        break;
-      default:
-        console.log('error');
-    }
-  };
-
+  let rate = Math.floor((current / target) * 100);
   const handleNicknameCheck = async () => {
     // 닉네임 중복 검사 api 호출
     try {
@@ -84,48 +68,55 @@ const ProfileModal = (props: { open?: any; close?: any }) => {
       {open ? (
         <section>
           <header>
-            프로필 수정
+            모금액 수령하기
             <button className={cx('close')} onClick={close}>
               &times;
             </button>
           </header>{' '}
           <main>
-            <div className={cx('title')}>닉네임 수정</div>
+            <div className={cx('total_fund_raised')}>
+              <div className={cx('data-row')}>
+                {' '}
+                <div className={cx('value-row')}>
+                  <div className={cx('value-title')}>
+                    <H4>총 모금액 </H4>
+                  </div>
+                  <H2>{String(target)}</H2>
+                  <H4>SSF</H4>
+                </div>
+                <div className={cx('people-row')}>
+                  <div className={cx('value-title')}>
+                    <H4>총</H4>
+                  </div>
+                  <H3>{String(totalpeople)}</H3>
+                  <H4> 명의 기부자</H4>
+                </div>
+                <Progressbar value={rate} />
+                <div className={cx('')}>
+                  <div className={cx('date-title')}>
+                    <Span color="gray">모금액 달성률 : </Span>{' '}
+                    <Span color="green">{String(rate).concat('%')}</Span>
+                    <Span color="gray"> (0.010212 SSF)</Span>{' '}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={cx('title')}>수령할 지갑 주소</div>
             <div className={cx('input')}>
               <div className={cx('inputWithBtn')}>
                 <Input
                   id="nickname"
                   value={nickname}
                   type="text"
-                  placeholder="닉네임"
-                  disabled={nicknameConfirm ? true : false}
-                  onChange={handleInput}
+                  disabled={true}
                 />
-                <div className={cx('inputBtn')}>
-                  <Button
-                    type="button"
-                    color="alternate"
-                    onClick={handleNicknameCheck}
-                    disabled={nicknameCheck ? false : true}
-                  >
-                    중복 검사
-                  </Button>
-                </div>
+                <div className={cx('inputBtn')}></div>
               </div>
-              {nicknameConfirm ? (
-                <p className={cx('validMsg')}>{nicknameMsg}</p>
-              ) : (
-                <p className={cx('invalidMsg')}>{nicknameMsg}</p>
-              )}
-            </div>
-            <div className={cx('title')}>프로필 사진 변경</div>
-            <div className={cx('avatar')}>
-              <Avatar size="large" />
             </div>
           </main>
           <footer>
-            <Button color="secondary" size="small" onClick={close}>
-              프로필 수정하기
+            <Button color="primary" size="large" onClick={close}>
+              수령하기
             </Button>
           </footer>
         </section>
@@ -134,4 +125,4 @@ const ProfileModal = (props: { open?: any; close?: any }) => {
   );
 };
 
-export default ProfileModal;
+export default FundModal;
