2a16d883a5ab98e6aaa953a4155170e257570470
eazae <xynezae@gmail.com>
2022-03-31 01:51:09 +0900

    [S06P22A404-230] feat: Doners NFT Contract instance

--- doners-frontend/src/services/web3.ts
+++ doners-frontend/src/services/web3.ts
@@ -1,5 +1,5 @@
 // ? [해결] 왜 .ts 파일에서는 import가 되지 않는가. js만 지원?
-import { DdABI } from 'assets/abi/DdABI';
+import { DdHelperABI } from 'assets/abi/DdHelperABI';
 import { FundraiserABI } from 'assets/abi/FundraiserABI';
 import { FundraiserFactoryABI } from 'assets/abi/FundraiserFactoryABI';
 import { SsfABI } from 'assets/abi/SsfABI';
@@ -25,38 +25,19 @@ export const SSFContract = new Web3Client.eth.Contract(
 export const FundraiserContract = (contractAddress: string) => {
   return new Web3Client.eth.Contract(FundraiserABI, contractAddress);
 };
+
 export const FundraiserFactoryContract = (contractAddress: string) => {
   return new Web3Client.eth.Contract(FundraiserFactoryABI, contractAddress);
 };
 
-//* NFT Contracts: Doners Doughnuts
-export const DDContract = () => {
-  return new Web3Client.eth.Contract(
-    DdABI,
-    process.env.REACT_APP_DD_CONTRACT_ADDRESS
-  );
-};
+//* NFT Contracts: Doners Doughnuts (DD)
+/* Contract instance for NFT editions  */
+// export const DDOwnershipContract = new Web3Client.eth.Contract(
+//   DdOwnershipABI,
+//   process.env.REACT_APP_DD_OWNERSHP_CONTRACT_ADDRESS
+// );
 
-/* Contract instance for 'covid' NFT editions  */
-export const DDCovidContract = new Web3Client.eth.Contract(
-  SsABI,
-  process.env.REACT_APP_DONERS_COVID_CONTRACT_ADDRESS
-);
-
-/* Contract instance for 'single' NFT editions  */
-export const DDSingleContract = new Web3Client.eth.Contract(
-  SsABI,
-  process.env.REACT_APP_DONERS_SINGLE_CONTRACT_ADDRESS
-);
-
-/* Contract instance for 'warrior' NFT editions  */
-export const DDWarriorContract = new Web3Client.eth.Contract(
-  SsfAI,
-  process.env.REACT_APP_DONERS_WARRIOR_CONTRACT_ADDRESS
-);
-
-/* Contract instance for 'patient' NFT editions  */
-export const DDPatientContract = new Web3Client.eth.Contract(
-  SsfAI,
-  process.env.REACT_APP_DONERS_PATIENT_CONTRACT_ADDRESS
+export const DDHelperContract = new Web3Client.eth.Contract(
+  DdHelperABI,
+  process.env.REACT_APP_DD_HELPER_CONTRACT_ADDRESS
 );
