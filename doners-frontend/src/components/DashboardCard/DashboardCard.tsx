a1f69f05bd79f4a946201fc5bf7f7d74da058971
ziu974 <ziu974@gmail.com>
2022-04-01 01:14:37 +0900

    [S06P22A404-232] feat: base AdminPage

---
+++ doners-frontend/src/components/DashboardCard/DashboardCard.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './DashboardCard.module.scss';
+
+const cx = classNames.bind(styles);
+
+const DashboardCard = () => {
+  return (
+    <div className={cx('card')}>
+      <div className={cx('p')}>
+        sldkj
+      </div>
+    </div>
+  )
+
+}
+
+export default DashboardCard;
---
+++ doners-frontend/src/containers/Admin/DashboardCard/DashboardCard.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './DashboardCard.module.scss';
+const cx = classNames.bind(styles);
+
+const DashboardCard = () => {
+  return (
+    <div className={cx('card')}>
+      <div className={cx('p')}>
+        sldkj
+      </div>
+    </div>
+  )
+
+}
+
+export default DashboardCard;
