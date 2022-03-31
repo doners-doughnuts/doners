d9e7a132f0f9a1d8898ab88a48a995bde4c49a59
eazae <xynezae@gmail.com>
2022-03-31 18:57:34 +0900

    [S06P22A404-229] feat: create NFT token with mint()

--- doners-frontend/src/services/blockchain/NftApi.ts
+++ doners-frontend/src/services/blockchain/NftApi.ts
@@ -19,6 +19,11 @@
 import chalk from 'chalk';
 import { DDHelperContract } from 'services/web3';
 import { MakeMinty } from './NftApiMintyTemp';
+//! metadata.json import 받아와서 loop 돌림
+// (https://nodejs.dev/learn/reading-files-with-nodejs)
+import fs from 'fs';
+import { Metadata } from 'assets/metadata/_ipfsMetadatas';
+const basePath = process.cwd();
 
 type NftApiProps = {};
 
@@ -30,9 +35,12 @@ enum NftEditions {
 }
 
 export const nftTest = async (walletAddress: string) => {
-  const result = await DDHelperContract.methods
-    .getTokensByOwner(walletAddress)
-    .call();
+  // const result = await DDHelperContract.methods
+  //   .getTokensByOwner(walletAddress)
+  //   .call();
+
+  // const result = await getUserNFTList(walletAddress);
+  const result = await DDHelperContract.methods.getTotalCnt().call();
   console.log('보유 NFT 목록: ', result);
 };
 
@@ -128,7 +136,62 @@ export const isMembership = async (walletAddress: string) => {
 // (https://ethereum.stackexchange.com/a/98495)
 export const getUserNFTList = async (walletAddress: string) => {
   const result = await DDHelperContract.methods.userOwnedTokens.call(
-    walletAddress
+    walletAddress,
+    function (err: any, res: any) {
+      console.log(res);
+    }
   );
   console.log('보유 NFT 목록: ', result);
 };
+
+// export const createDoughnut = async (metadata: Array<string>) => {
+export const createDoughnut = async () => {
+  console.log('CREATING DD TOKENS...');
+
+  /* 'Covid' EDITION */
+
+  for (let idx in Metadata) {
+    DDHelperContract.methods
+      .mint(Metadata[idx].metadata_uri)
+      .send({ from: '0xb72207EB8c21c7698d493Da3bB273F6C8a76E367' })
+      .then(console.log)
+      .catch(console.log);
+  }
+  // fs.readFile(
+  //   `${basePath}/web3/_ipfsMetadatas.json`,
+  //   'utf8',
+  //   (err: string, data: string) => {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     const metadata = JSON.parse(data);
+  //     /* Generate NFT with the metadata */
+  //     // parse data and RPC call for each token.
+  //     // console.log(metadata[idx])
+  //     // console.log(typeof metadata[idx].custom_fields.tokenId);
+  //     // const result =
+  //     // DDHelperContract.methods.createDoughnut(metadata[idx].custom_fields.tokenId, metadata[idx].metadata_uri).call().then(console.log);
+  //     // createDoughnut(metadata); //, metadata[idx].custom_fields.tokenId, metadata[idx].metadata_uri);
+  //     // for (idx in metadata) {
+  //     // DDHelperContract.methods.createDoughnut(parseInt(metadata[idx].custom_fields.tokenId), metadata[idx].metadata_uri).call().then(console.log);
+
+  //     // }
+  //     // console.log(result)
+  //     // }
+  //   }
+  // );
+  // DDHelperContract.methods.getMetadataUri(1000001).call().then(console.log).catch(console.log);
+};
+
+// const createDoughnut = async (metadata) => {
+//   // for (idx in metadata) {
+//   // DDHelperContract.methods.createDoughnut(parseInt(metadata[idx].custom_fields.tokenId), metadata[idx].metadata_uri).call().then(console.log);
+//   DDHelperContract.methods
+//     .mint(metadata[0].metadata_uri)
+//     .send({ from: '0xb72207EB8c21c7698d493Da3bB273F6C8a76E367' })
+//     .then(console.log)
+//     .catch(console.log);
+//   // }
+//   // DDHelperContract.methods.createDoughnut(tokenId, metadataUri).call().then(console.log).catch(console.log);
+// };
