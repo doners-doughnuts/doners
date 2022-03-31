75f7fdfe6d937e8bef8a6e8d17d161a85fe948fc
eazae <xynezae@gmail.com>
2022-04-01 01:14:37 +0900

    [S06P22A404-232] feat: base AdminPage

---
+++ doners-frontend/src/containers/Admin/ApprovalList/ApprovalList.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './ApprovalList.module.scss';
+const cx = classNames.bind(styles);
+
+const ApprovalList = () => {
+  return (
+    <div className={cx('card')}>
+      <div className={cx('p')}>
+        sldkj
+      </div>
+    </div>
+  )
+
+}
+
+export default ApprovalList;
---
+++ doners-frontend/src/containers/Admin/TransactionList/TransactionList.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './TransactionList.module.scss';
+const cx = classNames.bind(styles);
+
+const TransactionList = () => {
+  return (
+    <div className={cx('card')}>
+      <div className={cx('p')}>
+        sldkj
+      </div>
+    </div>
+  )
+
+}
+
+export default TransactionList;
