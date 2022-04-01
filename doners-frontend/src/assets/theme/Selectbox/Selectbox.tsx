6557842901599119b597f1a8b891431685f24cc1
sunmin <sunmlee126@gmail.com>
2022-04-01 14:26:24 +0900

    feat: donation apply form
    - evidence error
    - date format error

--- doners-frontend/src/assets/theme/Selectbox/Selectbox.tsx
+++ doners-frontend/src/assets/theme/Selectbox/Selectbox.tsx
@@ -3,19 +3,24 @@ import Span from 'assets/theme/Typography/Span/Span';
 import classNames from 'classnames/bind';
 import styles from './Selectbox.module.scss';
 import Select from 'react-select';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 const cx = classNames.bind(styles);
+
+type InputChangeEvent = ChangeEvent<HTMLInputElement>;
 
 type selectBoxType = {
   option: {
     value: string;
     label: string;
   }[];
+  onChange?: (...args: any[]) => void;
+  value?: string;
 };
 
-const Selectbox = ({ option }: selectBoxType) => {
+const Selectbox = ({ option, onChange, value }: selectBoxType) => {
   return (
     <div className={cx('box')}>
-      <Select options={option} defaultValue={option[0]} />
+      <Select options={option} defaultValue={option[0]} onChange={onChange} />
     </div>
   );
 };
