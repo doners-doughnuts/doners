d16b1879e3ba7a6267eb55a68efc418cb51dffe7
ziu974 <ziu974@gmail.com>
2022-04-01 13:28:20 +0900

    [S06P22A404-123] feat: title, content, label in DashboardCard, add utils/formatTime.ts

--- doners-frontend/src/components/DashboardCard/DashboardCard.tsx
+++ doners-frontend/src/components/DashboardCard/DashboardCard.tsx
@@ -1,15 +1,30 @@
+import H2 from 'assets/theme/Typography/H2/H2';
+import P from 'assets/theme/Typography/P/P';
 import classNames from 'classnames/bind';
 import styles from './DashboardCard.module.scss';
 
 const cx = classNames.bind(styles);
 
-const DashboardCard = () => {
+type DashboardCardType = {
+  header: string;
+  content: number;
+  label: string;
+}
+
+const DashboardCard = ({ header, content, label }: DashboardCardType) => {
+
+
   return (
     <div className={cx('card')}>
-      <div className={cx('p')}>
-        sldkj33
-      </div>
-    </div>
+      <P>{header}</P>
+      <span className={cx('card-content')}>
+
+        {content.toString()}
+        {/* <H2 >{content.toString()}</H2>
+        <P>{label}</P> */}
+      </span>
+      <span className={cx('card-content-label')}>{label}</span>
+    </div >
   )
 
 }
---
+++ doners-frontend/src/utils/formatTime.ts
@@ -0,0 +1,41 @@
+import { compareAsc, format, formatDistanceToNowStrict } from 'date-fns';
+import local from 'date-fns/locale/ko';
+// ----------------------------------------------------------------------
+
+// let date = new Date('2022-04-01T02:00:00Z');
+// console.log(date);
+// //Fri Apr 01 2022 11:00:00 GMT+0900 (Korean Standard Time)
+// // console.log(date.getFullYear() + '-' + date.getUTCMonth() + '-' + date.getDate())
+// console.log(
+//   date.toLocaleDateString('ko-KR').replaceAll(' ', '').replaceAll('.', '/')
+// );
+
+export function fDate(date: string) {
+  return format(new Date(date), 'yyyy/MM/dd');
+}
+
+export function fDateTime(date: string) {
+  return format(new Date(date), 'dd MMM yyyy HH:mm');
+}
+
+export function fDateTimeSuffix(date: string) {
+  //// return format(new Date(date), 'dd/MM/yyyy hh:mm p');
+  return format(new Date(date), 'yyyy/MM/dd p');
+}
+
+export function fToNow(date: string) {
+  //// return formatDistanceToNow(new Date(date), { addSuffix: true, });
+  return formatDistanceToNowStrict(new Date(date), {
+    locale: local,
+    addSuffix: true,
+  });
+}
+
+/**
+ * 모금가능한 기부인지 검사하는 함수
+ * @param {date} expire-date of the Donation
+ * @returns true: 마감된 기부 / false: 모금가능한 기부
+ */
+export function checkClosedDonation(date: string) {
+  return compareAsc(new Date(date), new Date()) === -1;
+}
