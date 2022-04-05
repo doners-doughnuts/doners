f34e12128ff145fd10641a05463515bc5aec85ad
ziu974 <ziu974@gmail.com>
2022-04-06 04:07:11 +0900

    [S06P22A404-76] feat: navigation alert UI

--- doners-frontend/src/components/Notification/NotificationItem/NotificationItem.tsx
+++ doners-frontend/src/components/Notification/NotificationItem/NotificationItem.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames/bind';
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { readNotification } from 'services/api/NotificationApi';
 import { NotificationItemType } from 'types/NotificationTypes';
 import styles from './NotificationItem.module.scss';
 
@@ -10,11 +12,26 @@ type NotificationItemProps = {
 };
 
 const NotificationItem = ({ item }: NotificationItemProps) => {
-  // const {} = props;
   const [isRead, setIsRead] = useState(false);
-  const [notification, setNotification] = useState<NotificationItemType>();
+  // const [notification, setNotification] = useState<NotificationItemType>();
+  const navigate = useNavigate();
 
-  return <>{item}</>;
+  const handleClick = async () => {
+    await readNotification(item.notificationId);
+    navigate(`/fundraisings/${item.donationId}`);
+  };
+
+  return (
+    <>
+      <div className={cx('item')} onClick={handleClick}>
+        <div className={cx('item-date')}>{item.createTime}</div>
+        {item.description}
+        <div className={cx('item-label')}>
+          {item.notificationIsRead ? '읽음' : '안읽음'}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotificationItem;
