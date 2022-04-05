a5eddf80794ef4e99955cbd4923d5348984414f4
thdalstn6352 <thdalstn6352@naver.com>
2022-04-05 17:42:14 +0900

    feat: 기부글 검색 기능 구현

---
+++ doners-frontend/src/containers/DonatePage/DonateListPage/DonateSearchBar/DonateSearchBar.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as SearchIcon } from 'assets/images/icon/search.svg';
+import Input from 'assets/theme/Input/Input';
+
+import classNames from 'classnames/bind';
+import { useRef } from 'react';
+import styles from './DonateSearchBar.module.scss';
+const cx = classNames.bind(styles);
+
+const DonateSearchBar = () => {
+  const inputRef: any = useRef();
+
+  const handleEnterClick = (e: any) => {
+    if (e.key === 'Enter') {
+      console.log('enter');
+      console.log(inputRef.current.value);
+    }
+  };
+  return (
+    <div className={cx('search-bar')} onKeyDown={handleEnterClick}>
+      <SearchIcon />
+      <input placeholder="검색어를 입력해주세요." ref={inputRef} />
+    </div>
+  );
+};
+
+export default DonateSearchBar;
