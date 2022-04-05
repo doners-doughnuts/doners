dba9dbbd945cb4d6fefa8d5244b9e42d0a8b9a88
ziu974 <ziu974@gmail.com>
2022-04-05 22:13:29 +0900

    [S06P22A404-241] feat: update Transaction Type
    - add checkWithdrawState() in FundingItem.tsx
    - update .env file with new contract address

--- doners-frontend/src/types/TransactionTypes.ts
+++ doners-frontend/src/types/TransactionTypes.ts
@@ -3,14 +3,15 @@ export type TransactionListItemType = {
   balance: number;
 };
 
-export type DonationTransactionType = {
-  account: string; // "0xb72207EB8c21c7698d493Da3bB273F6C8a76E367"
-  date: string; // "1649051426"
-  value: string; // "1"
-};
+// export type DonationTransactionType = {
+//   account: string; // "0xb72207EB8c21c7698d493Da3bB273F6C8a76E367"
+//   date: string; // "1649051426"
+//   value: string; // "1"
+// };
 
-export type DonationTransactionDetailType = {
-  account: string; // "0xb72207EB8c21c7698d493Da3bB273F6C8a76E367"
+export type DonationTransactionType = {
+  fromAccount: string; // "0xb72207EB8c21c7698d493Da3bB273F6C8a76E367"
+  toAccount: string;
   date: string; // "1649051426"
   value: string; // "1"
   donationId: string; // ""
