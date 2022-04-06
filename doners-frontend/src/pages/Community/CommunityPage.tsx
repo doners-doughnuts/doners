297125842e4510b4f1936fbaf5238ae06ee5e9dc
thdalstn6352 <thdalstn6352@naver.com>
2022-04-06 14:06:46 +0900

    fix: date 비교 오류 수정

--- doners-frontend/src/pages/Community/CommunityPage.tsx
+++ doners-frontend/src/pages/Community/CommunityPage.tsx
@@ -6,6 +6,11 @@ import CommunityTab from 'containers/CommunityPage/Tab/CommunityTab';
 import styles from '../page.module.scss';
 import classNames from 'classnames/bind';
 import Discord from 'containers/CommunityPage/Discord/Discord';
+import { isMembership } from 'services/blockchain/NftApi';
+import { getWalletAccount } from 'utils/walletAddress';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 const cx = classNames.bind(styles);
 
@@ -14,6 +19,26 @@ interface CommunityPageProps {
 }
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ focus }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    isHaveMembership();
+  }, []);
+
+  const isHaveMembership = async () => {
+    try {
+      const account = await getWalletAccount();
+      const exist = await isMembership(account);
+      if (!exist) {
+        toast.error('해당 페이지는 기부가 완료된 사람만 접근이 가능합니다.');
+        navigate('/');
+      }
+    } catch (error) {
+      toast.error('해당 페이지는 기부가 완료된 사람만 접근이 가능합니다.');
+      navigate('/');
+    }
+  };
+
   return (
     <section className={cx('container')}>
       <div className={cx('row')}>
@@ -30,7 +55,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ focus }) => {
         <Epilogue />
       ) : focus === 3 ? (
         <BoardList />
-      ) : focus == 4 ? (
+      ) : focus === 4 ? (
         <Discord />
       ) : null}
     </section>
