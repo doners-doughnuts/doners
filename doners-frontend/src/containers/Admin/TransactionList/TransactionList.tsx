2435e5fd9339ced5ec349f29f82b242fbcbe6cc1
eazae <xynezae@gmail.com>
2022-04-06 17:18:30 +0900

    fix: notification link direction

--- doners-frontend/src/containers/Admin/TransactionList/TransactionList.tsx
+++ doners-frontend/src/containers/Admin/TransactionList/TransactionList.tsx
@@ -29,36 +29,19 @@ const TransactionList = ({ transactionList }: any) => {
       </div>
       <div className={cx('inner-container')}>
         <img src={reciept} alt="background" />
-        {/* <div className={cx('vl')}></div> */}
+        <div className={cx('vl')}></div>
         <div className={cx('content')}>
           {transactionList.length > 0
             ? transactionList
                 .slice(0, transactionList.length - 1)
                 .map((item: any, idx: number) => (
-                  <TransactionListItem key={idx} item={item} />
+                  <>
+                    <TransactionListItem key={idx} item={item} />
+                    {/* <TransactionListItem key={idx * 12} item={item} /> */}
+                    {/* <TransactionListItem key={idx * 13} item={item} /> */}
+                  </>
                 ))
             : null}
-
-          {transactionList.length > 0 &&
-            transactionList.map((data: any, idx: number) => {
-              console.log(data);
-              return (
-                <div className={cx('history-item')} key={idx}>
-                  <div className={cx('history-row')}>
-                    <div className={cx('from-account')}>
-                      <P>{data.fromAccount}</P>
-                      {/* <Span>{fToNow(fFundraiserContractTime(data.date))}</Span> */}
-                    </div>
-                  </div>
-                  <div className={cx('money')}>
-                    <Tag color="green">{`${data.value} SSF`}</Tag>
-                    {/* <Tag>{`${data.value}SSF`}</Tag> */}
-                    {/* <P>{data.value}</P> */}
-                    {/* <P>SSF</P> */}
-                  </div>
-                </div>
-              );
-            })}
         </div>
       </div>
     </div>
