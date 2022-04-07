31498a2e8be8a479488af75bff37caaf8127fdf2
thdalstn6352 <thdalstn6352@naver.com>
2022-04-08 08:51:42 +0900

    refactor: landing page UI 수정

--- doners-frontend/src/containers/HomePage/AboutOurTeam/AboutOurTeam.tsx
+++ doners-frontend/src/containers/HomePage/AboutOurTeam/AboutOurTeam.tsx
@@ -22,64 +22,64 @@ const AboutOurTeam = () => {
     AOS.init();
   }, []);
   return (
-    <div>
-      <section className={cx('About', 'section')}>
+    <div className={cx('About', 'section')}>
+      <div className={cx('title')}>
         <H1>About our Team</H1>
-        <div className={cx('teammate1', 'team-character')}>
-          <img data-aos="zoom-out" src={sunmin} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>FE</Tag>
-            <div className={cx('name')}>이선민</div>
-            <div className={cx('id')}>@sunmin7</div>
-          </div>
+      </div>
+      <div className={cx('teammate1', 'team-character')}>
+        <img data-aos="zoom-out" src={sunmin} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>FE</Tag>
+          <div className={cx('name')}>이선민</div>
+          <div className={cx('id')}>@sunmin7</div>
         </div>
-        <div className={cx('teammate2', 'team-character')}>
-          <img data-aos="zoom-out" src={minsu} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>FE</Tag>
-            <div className={cx('name')}>송민수</div>
-            <div className={cx('id')}>@thdalstn6352</div>
-          </div>
+      </div>
+      <div className={cx('teammate2', 'team-character')}>
+        <img data-aos="zoom-out" src={minsu} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>FE</Tag>
+          <div className={cx('name')}>송민수</div>
+          <div className={cx('id')}>@thdalstn6352</div>
         </div>
-        <div className={cx('teammate3', 'team-character')}>
-          <img data-aos="zoom-out" src={jiwu} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>FE, BlockChain</Tag>
-            <div className={cx('name')}>신지우</div>
-            <div className={cx('id')}>@ziu974</div>
-          </div>
+      </div>
+      <div className={cx('teammate3', 'team-character')}>
+        <img data-aos="zoom-out" src={jiwu} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>FE, BlockChain</Tag>
+          <div className={cx('name')}>신지우</div>
+          <div className={cx('id')}>@ziu974</div>
         </div>
-        <div className={cx('teammate4', 'team-character')}>
-          <img data-aos="zoom-out" src={kibum} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>BE</Tag>
-            <div className={cx('name')}>박기범</div>
-            <div className={cx('id')}>@kibum414</div>
-          </div>
+      </div>
+      <div className={cx('teammate4', 'team-character')}>
+        <img data-aos="zoom-out" src={kibum} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>BE</Tag>
+          <div className={cx('name')}>박기범</div>
+          <div className={cx('id')}>@kibum414</div>
         </div>
-        <div className={cx('teammate5', 'team-character')}>
-          <img data-aos="zoom-out" src={changhyeon} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>Leader, BE</Tag>
+      </div>
+      <div className={cx('teammate5', 'team-character')}>
+        <img data-aos="zoom-out" src={changhyeon} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>Leader, BE</Tag>
 
-            <div className={cx('name')}>손창현</div>
-            <div className={cx('id')}>@gganzii1215</div>
-          </div>
+          <div className={cx('name')}>손창현</div>
+          <div className={cx('id')}>@gganzii1215</div>
         </div>
-        <div className={cx('teammate6', 'team-character')}>
-          <img data-aos="zoom-out" src={hongjin} alt="character" />
-          <div className={cx('desc')}>
-            <Tag color={'black'}>BE, BlockChain</Tag>
-            <div className={cx('name')}>정홍진</div>
-            <div className={cx('id')}>@mistake9518</div>
-          </div>
+      </div>
+      <div className={cx('teammate6', 'team-character')}>
+        <img data-aos="zoom-out" src={hongjin} alt="character" />
+        <div className={cx('desc')}>
+          <Tag color={'black'}>BE, BlockChain</Tag>
+          <div className={cx('name')}>정홍진</div>
+          <div className={cx('id')}>@mistake9518</div>
         </div>
+      </div>
 
-        <div className={cx('background')}></div>
-        <div>
-          <span className={cx('teamName')}>든킨</span>
-        </div>
-      </section>
+      <div className={cx('background')}></div>
+      <div>
+        <span className={cx('teamName')}>든킨</span>
+      </div>
     </div>
   );
 };
